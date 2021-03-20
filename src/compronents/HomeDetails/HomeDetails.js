992d68fdaf6b2eb4ce93f49084e3a5db3a2a17aa
rubelahme <rubelahme3303@gmail.com>
2021-03-20 19:56:11 +0600

    ato khothin ar assinment den na vai

--- src/compronents/HomeDetails/HomeDetails.js
+++ src/compronents/HomeDetails/HomeDetails.js
@@ -3,7 +3,7 @@ import React from "react";
 const HomeDetails = () => {
   return (
     <div className="text-center text-success pt-5 mt-5">
-      <h1>Please Click Home Menu and view Details....</h1>
+      <h1>Please Click Home Menu And view World Best Car Details....</h1>
     </div>
   );
 };
